ba4976c19f61984e135af75b00df8610086b8899
Henry Adagala <adagalahenry@gmail.com>
2022-06-26 19:21:41 +0300

    update configs

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,31 +1,41 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
-  rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'google',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.dev.json'],
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: [
+        '/lib/**/*', // Ignore built files.
+    ],
+    plugins: ['@typescript-eslint', 'import'],
+    rules: {
+        quotes: ['error', 'single'],
+        'quote-props': ['error', 'as-needed'],
+        'import/no-unresolved': 0,
+        'object-curly-spacing': ['error', 'always'],
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+                ignoredNodes: ['ConditionalExpression'],
+            },
+        ],
+        'max-len': ['error', { code: 140 }],
+        'comma-dangle': ['error', 'only-multiline'],
+    },
 };
