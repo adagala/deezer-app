2217522273c91587f602518a908f4d4630a1b703
Henry Adagala <adagalahenry@gmail.com>
2022-06-27 13:23:12 +0300

    search for tracks

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,20 @@
-const Search = () => {
-  return <div>Tracks search</div>;
+const Search = ({
+  title,
+  onChange,
+  onSearchTracks,
+}: {
+  title: string;
+  onChange: (e: any) => void;
+  onSearchTracks: () => Promise<void>;
+}) => {
+  return (
+    <div>
+      <input value={title} type="text" onChange={onChange} />
+      <button onClick={onSearchTracks} type="button">
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Search;
